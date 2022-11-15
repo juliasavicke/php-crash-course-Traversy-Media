1e333f156b4f8eea6090535aaa72749ff7191c95
juliasavicke <juliasavicke@gmail.com>
2022-11-15 15:33:08 +0200

    numbers

--- 03_numbers.php
+++ 03_numbers.php
@@ -25,13 +25,36 @@ echo $a++; //first the $a is printed and then increased
 echo ++$a; //first increased and then printed
 
 // Decrement operator
+echo $a-- . '<br>';
+echo --$a . '<br>';
 
 // Number checking functions
+is_float(1.25);
+is_double(1.25);
+is_int(5);
+is_numeric("3.45"); //true
+is_numeric('3g.45'); //false
 
 // Conversion
 
+$strNumber = '12.34';
+$number = intval($strNumber);
+var_dump($number);
+echo '<br>';
+
 // Number functions
+abs(-15) . '<br>';
+pow(2, 3) . '<br>';
+sqrt(16) . '<br>';
+max(16, 12) . '<br>';
+min(16, 12) . '<br>';
+round(2.4) . '<br>';
+floor(16.7) . '<br>';
+ceil(16.7) . '<br>';
+
 
 // Formatting numbers
+$number = 176865432.234;
+echo number_format($number, 2, ',', ' ');
 
 // https://www.php.net/manual/en/ref.math.php
