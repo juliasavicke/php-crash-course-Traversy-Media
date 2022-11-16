8a6f04068ed41f6ca344c350ec64c9698145b5c0
juliasavicke <juliasavicke@gmail.com>
2022-11-16 15:54:52 +0200

    search products

--- 14_product_crud/index.php
+++ 14_product_crud/index.php
@@ -2,7 +2,14 @@
 $pdo = new PDO('mysql:host=localhost;port=3306;dbname=products_crud', 'root', '');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+
+$search = $_GET['search'] ?? '';
+if ($search) {
+    $statement = $pdo->prepare('SELECT * FROM products WHERE title LIKE :title ORDER BY create_date DESC');
+    $statement->bindValue(':title', "%$search%");
+} else {
+    $statement = $pdo->prepare('SELECT * FROM products ORDER BY create_date DESC');
+}
 $statement->execute();
 $products = $statement->fetchAll(PDO::FETCH_ASSOC);
 // echo '<pre>';
@@ -26,6 +33,16 @@ $products = $statement->fetchAll(PDO::FETCH_ASSOC);
     <p>
         <a href="create.php" class="btn btn-success">Create Product</a>
     </p>
+
+    <form action="">
+        <div class="input-group mb-3">
+            <input type="text" class="form-control" placeholder="Search for products" name="search" value="<?php echo $search; ?>">
+            <div class="input-group-append">
+                <button class="btn btn-outline-secondary" type="submit">Search</button>
+            </div>
+        </div>
+    </form>
+
     <table class="table">
         <thead>
             <tr>
