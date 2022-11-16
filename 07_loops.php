4f7163edd24450c5044ad9b71f38799baac871ba
juliasavicke <juliasavicke@gmail.com>
2022-11-16 08:26:04 +0200

    loops

--- 07_loops.php
+++ 07_loops.php
@@ -2,12 +2,42 @@
 
 // while
 
+
 // Loop with $counter
+$counter = 0;
+while ($counter < 10) {
+    echo $counter . '<br>';
+    $counter++;
+}
 
 // do - while
+do {
+    echo $counter;
+    $counter++;
+} while ($counter < 10);
 
 // for
-
+for ($i = 0; $i < 10; $i++) {
+    echo $i . '<br>';
+}
+$fruits = ['Bannana', 'Apple', 'Orange'];
 // foreach
+foreach ($fruits as $i => $fruit) {
+    echo $i . ' ' . $fruit . '<br>';
+}
 
 // Iterate Over associative array.
+$person = [
+    'name' => 'Brad',
+    'surname' => 'Traversy',
+    'age' => 30,
+    'hobbies' => ['Tennis', 'Basketball', 'Video Games'],
+];
+
+foreach ($person as $key => $value) {
+    if (is_array($value)) {
+        echo $key . ' ' . implode(', ', $value) . '<br>';
+    } else {
+        echo $key . ' ' . $value . '<br>';
+    }
+}
